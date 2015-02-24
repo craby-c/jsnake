651649197202c7ccefdd00f891119ebdae8fd708
craby-c <securicula@gmail.com>
2015-02-24 19:39:39 +0700

    remove double

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 # Snake
 Simple snake game in Javascript
-=======
-# jsnake
-Simple game "Snake" on JavaScript.
->>>>>>> d8a6136fe7706cc0398947f184f00bd42387c30d
--- snake.js
+++ snake.js
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
-speed = 9;
-size = 15;
-=======
 speed = 1;
 size = 10;
 food_number = 5;
 
->>>>>>> d8a6136fe7706cc0398947f184f00bd42387c30d
 function init(){
   x = 0;
   y = 0;
@@ -15,10 +10,6 @@ function init(){
   snake_length = 1;
   snake_body = ["0_0"];
   food = [];
-<<<<<<< HEAD
-  food_number = 5;
-=======
->>>>>>> d8a6136fe7706cc0398947f184f00bd42387c30d
   setScore();
   setSpeed();
   setSize();
@@ -29,11 +20,7 @@ function start(){
   init();
   buttonChange();
   document.body.onkeydown = khandle;
-<<<<<<< HEAD
-  loop = window.setInterval(main, 1000-100*speed);
-=======
   loop = window.setInterval(main, 500-50*speed);
->>>>>>> d8a6136fe7706cc0398947f184f00bd42387c30d
 }
 
 function main(){
@@ -43,33 +30,10 @@ function main(){
     showSnake();
     setScore();
     setSpeed();
-<<<<<<< HEAD
-    // robo();
-=======
->>>>>>> d8a6136fe7706cc0398947f184f00bd42387c30d
     step();
     gameOver();
 }
 
-<<<<<<< HEAD
-function robo(){
-  if (x<5){
-    x++;
-  }
-  else{
-    y++;
-  }
-}
-
-function khandle(e) {
-  if(e.keyCode == 37){
-    dy = -1;
-    dx = 0;
-  }
-  if(e.keyCode == 39){
-    dy = 1;
-    dx = 0;
-=======
 function khandle(e) {
   if(e.keyCode == 37){
     dx = 0;
@@ -78,7 +42,6 @@ function khandle(e) {
   if(e.keyCode == 39){
     dx = 0;
     dy = 1;
->>>>>>> d8a6136fe7706cc0398947f184f00bd42387c30d
   }
   if(e.keyCode == 38){
     dx = -1;
@@ -107,34 +70,12 @@ function step(){
   }
   var head = x + "_" + y;
   snake_body.push(head);
-<<<<<<< HEAD
-  for (i in food){
-=======
   for (var i in food){
->>>>>>> d8a6136fe7706cc0398947f184f00bd42387c30d
     if (head == food[i]){
       snake_length++;
       food.splice(i,1);
     }
   }
-<<<<<<< HEAD
-}
-
-function setScore(){
-  document.getElementById("score").innerHTML = "Score: " + (snake_length-1);
-}
-
-function increaseSpeed(){
-  if(speed < 9){
-    speed++;
-  }
-  setSpeed();
-}
-
-function decreaseSpeed(){
-  if(1 < speed){
-    speed--;
-=======
   if (snake_length < snake_body.length){
     snake_body.shift();
   }
@@ -144,56 +85,22 @@ function changeSpeed(x){
   speed += x;
   if(speed < 1 || speed > 9){
     speed -= x;
->>>>>>> d8a6136fe7706cc0398947f184f00bd42387c30d
   }
   setSpeed();
 }
 
-<<<<<<< HEAD
-function increaseSize(){
-  if(size < 15){
-    size++;
-  }
-  setSize();
-  document.getElementById("table").removeChild(t);
-  createField();
-}
-
-function decreaseSize(){
-  if(5 < size){
-    size--;
-=======
 function changeSize(x){
   size += x;
   if(size < 5 || size > 15){
     size -= x;
->>>>>>> d8a6136fe7706cc0398947f184f00bd42387c30d
   }
   setSize();
   document.getElementById("table").removeChild(t);
   createField();
 }
 
-<<<<<<< HEAD
-function setSpeed(){
-  document.getElementById("speed").innerHTML = "Speed: " + (speed);
-}
-
-function setSize(){
-  document.getElementById("size").innerHTML = "Size: " + (size);
-}
-
-function showSnake(){
-  if (snake_length < snake_body.length){
-    snake_body.shift();
-  }
-  for (i in snake_body){
-    setBlue(snake_body[i]);
-  }
-=======
 function showSnake(){
   snake_body.forEach(setBlue);
->>>>>>> d8a6136fe7706cc0398947f184f00bd42387c30d
 }
 
 function getRandom(){
@@ -205,20 +112,12 @@ function makeFood(){
     var food_place = getRandom() + "_" + getRandom();
     var infood = false;
     var insnake = false;
-<<<<<<< HEAD
-    for (i in food){
-=======
     for (var i in food){
->>>>>>> d8a6136fe7706cc0398947f184f00bd42387c30d
       if (food_place == food[i]){
         infood = true;
       }
     }
-<<<<<<< HEAD
-    for (i in snake_body){
-=======
     for (var i in snake_body){
->>>>>>> d8a6136fe7706cc0398947f184f00bd42387c30d
       if (food_place == snake_body[i]){
         insnake = true;
       }
@@ -230,20 +129,6 @@ function makeFood(){
 }
 
 function showFood(){
-<<<<<<< HEAD
-  for (i in food){
-    setGreen(food[i]);
-  }
-}
-
-function gameOver(){
-  for (i in snake_body){
-    if (snake_body.length > 2){
-        if (snake_body[i-1] == snake_body[snake_body.length-1]){
-	      stop();
-          alert("Game Over");
-	    }
-=======
   food.forEach(setGreen);
 }
 
@@ -253,41 +138,20 @@ function gameOver(){
     if (snake_body[i-1] == l){
 	  stop();
       alert("Game Over");
->>>>>>> d8a6136fe7706cc0398947f184f00bd42387c30d
 	}
   }
 }
 
 function setBlue(name){
   var b = document.getElementsByClassName(name)[0];
-<<<<<<< HEAD
-  b.style.background = "DodgerBlue";
-=======
   // b.style.background = "DodgerBlue";
   var st = b.className.split(" ");
   b.className = st[0] + " " + st[1] + " snake";
   console.log(b.className);
->>>>>>> d8a6136fe7706cc0398947f184f00bd42387c30d
 }
 
 function setGreen(name){
   var g = document.getElementsByClassName(name)[0];
-<<<<<<< HEAD
-  g.style.background = "limegreen";
-}
-
-function removeColor(name){
-  var r = document.getElementsByClassName(name)[0];
-  r.style.background = "white";
-}
-
-function clearField(){
-  var c = document.getElementsByClassName("Cell");
-  for (i=0; i<c.length; i++){
-    var cel = c[i];
-    cel.style.background = "white";
-  };
-=======
   // g.style.background = "limegreen";
   var st = g.className.split(" ");
   g.className = st[0] + " " + st[1] + " food";
@@ -314,23 +178,15 @@ function clearField(){
   	// console.log(c[i].className);
     // c[i].style.background = "none";
   }
->>>>>>> d8a6136fe7706cc0398947f184f00bd42387c30d
 }
 
 function createField(){
   t = document.createElement('table');
   var tbody = document.createElement('tbody');
-<<<<<<< HEAD
-  for(i=0;i<size;i++) {
-    var row = document.createElement('div');
-    row.className = "row";
-    for (j=0;j<size;j++){
-=======
   for(var i = 0; i < size; i++) {
     var row = document.createElement('div');
     row.className = "row";
     for (var j = 0; j < size; j++){
->>>>>>> d8a6136fe7706cc0398947f184f00bd42387c30d
       var cell = document.createElement('div');
       cell.className = "cell "+i+"_"+j;
       row.appendChild(cell);
